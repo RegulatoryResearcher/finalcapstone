cc1266ce2c59f26366a923ffd0e98c500476fbdb
RegulatoryResearcher <138043571+RegulatoryResearcher@users.noreply.github.com>
2024-03-11 20:44:45 +0000

    first commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#We will create a variable "name" and ask the user to input this information
+#We will the print this input ie our variable name
+#This is repeated for age
+#Using the print function we display Hello World
+name = input("Please enter you name: ")
+print(name)
+age = input("What age are you?: ")
+print(age)
+print("Hello World")
